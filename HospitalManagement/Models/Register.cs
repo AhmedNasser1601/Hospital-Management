cb9b884227c691f4008ba5af6185dbe79e185dac
Ahmed Nasser <ahmednasser1601@gmail.com>
2023-12-31 00:38:25 +0200

    Create Register.cs

---
+++ HospitalManagement/Models/Register.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace HospitalManagement.Models
+{
+    public class Register
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class RegisterValidator : AbstractValidator<Register>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Enter Name: ").Length(2, 10);
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Enter Email: ").EmailAddress().WithMessage("Enter Valid Email: ");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Enter Password: ").Length(6, 20);
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Enter Confirm Password: ").Equal(x => x.Password).WithMessage("Password and Confirm Password must be same");
+        }
+    }
+}
