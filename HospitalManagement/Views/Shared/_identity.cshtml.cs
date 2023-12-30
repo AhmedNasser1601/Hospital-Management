cc2e8c20757f74cdb42763e9d9720799eeb61b1c
Ahmed Nasser <ahmednasser1601@gmail.com>
2023-12-31 00:36:04 +0200

    Views/Shared

---
+++ HospitalManagement/Views/Shared/_identity.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HospitalManagement.Views.Shared
+{
+    public class _identityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
