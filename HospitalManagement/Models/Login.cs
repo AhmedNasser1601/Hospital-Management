16d4a5a9ad10857d84767580c8a90fe3db6a303b
Ahmed Nasser <ahmednasser1601@gmail.com>
2023-12-31 00:38:22 +0200

    Create Login.cs

---
+++ HospitalManagement/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace HospitalManagement.Models
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginValidator : AbstractValidator<Login>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Enter Email: ").EmailAddress().WithMessage("Enter Valid Email: ");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Enter Password: ").Length(6, 20);
+        }
+    }
+}
