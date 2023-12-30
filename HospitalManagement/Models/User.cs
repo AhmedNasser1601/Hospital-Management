771a9939252f8ecf1f75d13e952d4925e2fa8edc
Ahmed Nasser <ahmednasser1601@gmail.com>
2023-12-31 00:38:28 +0200

    Create User.cs

---
+++ HospitalManagement/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagement.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder) { }
+    }
+
+    public class UserRole
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
