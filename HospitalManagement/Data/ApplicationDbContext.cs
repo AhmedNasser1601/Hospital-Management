23dc92857e192c167ca7010dff858db5988b13b5
Ahmed Nasser <ahmednasser1601@gmail.com>
2023-12-31 00:38:02 +0200

    Create ApplicationDbContext.cs

---
+++ HospitalManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HospitalManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace HospitalManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
