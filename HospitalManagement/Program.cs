0278a88a0cf4045b24e47dc418792ace63cdcf12
Ahmed Nasser <ahmednasser1601@gmail.com>
2023-12-31 02:04:48 +0200

    Update Program.cs

--- HospitalManagement/Program.cs
+++ HospitalManagement/Program.cs
@@ -49,7 +49,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Appointment}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 await DbInit.SeedUsersAndRolesAsync(app);
